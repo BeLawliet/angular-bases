3f6aa62d40cea14a4d8a60f1211736b040c6dc4f
BeLawliet <elcostexd995@gmail.com>
2023-04-23 16:20:04 -0500

    feat: Implemented DBZ Services and Components

--- src/app/dbz/components/character/character.component.ts
+++ src/app/dbz/components/character/character.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'dbz-character',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./character.component.css']
 })
 export class CharacterComponent {
+  @Output()
+  public onNewCharacter: EventEmitter<Character> = new EventEmitter();
 
+  public character: Character = {id: '', name: '', power: 0};
+
+  emitCharacter(): void {
+    if (this.character.name.length === 0) return;
+    const newCharacter = {id: uuid(), name: this.character.name, power: this.character.power};
+    this.onNewCharacter.emit(newCharacter);
+    this.character = {id: '', name: '', power: 0};
+  }
 }
